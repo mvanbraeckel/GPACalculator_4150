d539aa787199a708cc6cbe255ed74efbaef3897d
mvanbraeckel <mvanbrae@uoguelph.ca>
2021-10-17 11:10:26 -0400

    finished java and python tests, cleaned pycache

--- A1-GPA-Calculator/java-GPA-calculator/TestConversions.java
+++ A1-GPA-Calculator/java-GPA-calculator/TestConversions.java
@@ -1,19 +1,177 @@
+/**
+ * Course: Software Reliability and Testing (CIS*4150) - Assignment 1
+ * Date: October 17, 2021
+ * Author: Mitchell Van Braeckel (1002297, mvanbrae@uoguelph.ca)
+ */
+
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import static org.junit.Assert.assertEquals;
 
 import GPAcalculator.*;
 
+/**
+ * Tests the conversion of percentage grades to letter values for each letter's low, midrange, and high values
+ */
 public class TestConversions
 {
-	// Set up anything that you need prior to the tests in the @Before
-	// declaration area
+    // Set up anything that you need prior to the tests in the @Before
+    // declaration area
+    static GPAconverterTool gpaConverter = null;
 
-	@Before public void setup() {
+    @BeforeClass public static void setUpClass() {
+		gpaConverter = GPAconverterTool.getReference();
 	}
 
-	@Test public void test100percent() {
-		// do a test
-	}
+    // @Before public void setup() {
+    //     this.gpaConverter = GPAconverterTool.getReference();
+    // }
 
+    // @Test public void test100percent() {
+    //     assertEquals(this.gpaConverter.getLetterForNumericGrade(100), "A+");
+    // }
+
+    // "Outstanding",		4.3f,	"A+",		90,	100	);
+    @Test public void test100percent__A_plus__high() {
+        assertEquals("A+", this.gpaConverter.getLetterForNumericGrade(100));
+    }
+    @Test public void test095percent__A_plus__midrange() {
+        assertEquals("A+", this.gpaConverter.getLetterForNumericGrade(95));
+    }
+    @Test public void test090percent__A_plus__low() {
+        assertEquals("A+", this.gpaConverter.getLetterForNumericGrade(90));
+    }
+
+    // "Excellent",			4.0f,	"A",		85,	89	);
+    @Test public void test089percent__A__high() {
+        assertEquals("A", this.gpaConverter.getLetterForNumericGrade(89));
+    }
+    @Test public void test087percent__A__midrange() {
+        assertEquals("A", this.gpaConverter.getLetterForNumericGrade(87));
+    }
+    @Test public void test085percent__A__low() {
+        assertEquals("A", this.gpaConverter.getLetterForNumericGrade(85));
+    }
+
+    // "Very Good",			3.7f,	"A-",		80,	84	);
+    @Test public void test084percent__A_minus__high() {
+        assertEquals("A-", this.gpaConverter.getLetterForNumericGrade(84));
+    }
+    @Test public void test082percent__A_minus__midrange() {
+        assertEquals("A-", this.gpaConverter.getLetterForNumericGrade(82));
+    }
+    @Test public void test080percent__A_minus__low() {
+        assertEquals("A-", this.gpaConverter.getLetterForNumericGrade(80));
+    }
+
+    // "Good",				3.3f,	"B+",		77,	79	);
+    @Test public void test079percent__B_plus__high() {
+        assertEquals("B+", this.gpaConverter.getLetterForNumericGrade(79));
+    }
+    @Test public void test078percent__B_plus__midrange() {
+        assertEquals("B+", this.gpaConverter.getLetterForNumericGrade(78));
+    }
+    @Test public void test077percent__B_plus__low() {
+        assertEquals("B+", this.gpaConverter.getLetterForNumericGrade(77));
+    }
+
+    // "Good",				3.0f,	"B",		73,	76	);
+    @Test public void test076percent__B__high() {
+        assertEquals("B", this.gpaConverter.getLetterForNumericGrade(76));
+    }
+    @Test public void test075percent__B__midrange() {
+        assertEquals("B", this.gpaConverter.getLetterForNumericGrade(75));
+    }
+    @Test public void test073percent__B__low() {
+        assertEquals("B", this.gpaConverter.getLetterForNumericGrade(73));
+    }
+
+    // "Good",				2.7f,	"B-",		70,	72	);
+    @Test public void test072percent__B_minus__high() {
+        assertEquals("B-", this.gpaConverter.getLetterForNumericGrade(72));
+    }
+    @Test public void test071percent__B_minus__midrange() {
+        assertEquals("B-", this.gpaConverter.getLetterForNumericGrade(71));
+    }
+    @Test public void test070percent__B_minus__low() {
+        assertEquals("B-", this.gpaConverter.getLetterForNumericGrade(70));
+    }
+
+    // "Satisfactory",		2.3f,	"C+",		67,	69	);
+    @Test public void test069percent__C_plus__high() {
+        assertEquals("C+", this.gpaConverter.getLetterForNumericGrade(69));
+    }
+    @Test public void test068percent__C_plus__midrange() {
+        assertEquals("C+", this.gpaConverter.getLetterForNumericGrade(68));
+    }
+    @Test public void test067percent__C_plus__low() {
+        assertEquals("C+", this.gpaConverter.getLetterForNumericGrade(67));
+    }
+
+    // "Satisfactory",		2.0f,	"C",		63,	66	);
+    @Test public void test066percent__C__high() {
+        assertEquals("C", this.gpaConverter.getLetterForNumericGrade(66));
+    }
+    @Test public void test065percent__C__midrange() {
+        assertEquals("C", this.gpaConverter.getLetterForNumericGrade(65));
+    }
+    @Test public void test063percent__C__low() {
+        assertEquals("C", this.gpaConverter.getLetterForNumericGrade(63));
+    }
+
+    // "Satisfactory",		1.7f,	"C-",		60,	62	);
+    @Test public void test062percent__C_minus__high() {
+        assertEquals("C-", this.gpaConverter.getLetterForNumericGrade(62));
+    }
+    @Test public void test061percent__C_minus__midrange() {
+        assertEquals("C-", this.gpaConverter.getLetterForNumericGrade(61));
+    }
+    @Test public void test060percent__C_minus__low() {
+        assertEquals("C-", this.gpaConverter.getLetterForNumericGrade(60));
+    }
+
+    // "Marginal Pass",		1.3f,	"D+",		57,	59	);
+    @Test public void test059percent__D_plus__high() {
+        assertEquals("D+", this.gpaConverter.getLetterForNumericGrade(59));
+    }
+    @Test public void test058percent__D_plus__midrange() {
+        assertEquals("D+", this.gpaConverter.getLetterForNumericGrade(58));
+    }
+    @Test public void test057percent__D_plus__low() {
+        assertEquals("D+", this.gpaConverter.getLetterForNumericGrade(57));
+    }
+
+    // "Marginal Pass",		1.0f,	"D",		53,	56	);
+    @Test public void test056percent__D__high() {
+        assertEquals("D", this.gpaConverter.getLetterForNumericGrade(56));
+    }
+    @Test public void test055percent__D__midrange() {
+        assertEquals("D", this.gpaConverter.getLetterForNumericGrade(55));
+    }
+    @Test public void test053percent__D__low() {
+        assertEquals("D", this.gpaConverter.getLetterForNumericGrade(53));
+    }
+
+    // "Marginal Pass",		0.7f,	"D-",		50,	52	);
+    @Test public void test052percent__D_minus__high() {
+        assertEquals("D-", this.gpaConverter.getLetterForNumericGrade(52));
+    }
+    @Test public void test051percent__D_minus__midrange() {
+        assertEquals("D-", this.gpaConverter.getLetterForNumericGrade(51));
+    }
+    @Test public void test050percent__D_minus__low() {
+        assertEquals("D-", this.gpaConverter.getLetterForNumericGrade(50));
+    }
+
+    // "Failure",			0.0f,	"F",		0,	49	);
+    @Test public void test049percent__F__high() {
+        assertEquals("F", this.gpaConverter.getLetterForNumericGrade(49));
+    }
+    @Test public void test025percent__F__midrange() {
+        assertEquals("F", this.gpaConverter.getLetterForNumericGrade(25));
+    }
+    @Test public void test000percent__F__low() {
+        assertEquals("F", this.gpaConverter.getLetterForNumericGrade(0));
+    }
 }
--- A1-GPA-Calculator/python-GPA-calculator/runtests.py
+++ A1-GPA-Calculator/python-GPA-calculator/runtests.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+Course: Software Reliability and Testing (CIS*4150) - Assignment 1
+Date: October 17, 2021
+Author: Mitchell Van Braeckel (1002297, mvanbrae@uoguelph.ca)
+"""
+
 import gpa_calculator
 import unittest
 
@@ -9,133 +15,133 @@ import unittest
 class TestPercentageGradeToLetterConversion(unittest.TestCase):
 
     # "Outstanding",        4.3,    "A+",        90,    100    ))
-    def test__A_plus__high(self):
+    def test100percent__A_plus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(100), "A+")
 
-    def test__A_plus__midrange(self):
+    def test095percent__A_plus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(95), "A+")
 
-    def test__A_plus__low(self):
+    def test090percent__A_plus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(90), "A+")
 
     # "Excellent",        4.0,    "A",        85,    89    ))
-    def test__A__high(self):
+    def test089percent__A__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(89), "A")
 
-    def test__A__midrange(self):
+    def test087percent__A__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(87), "A")
 
-    def test__A__low(self):
+    def test085percent__A__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(85), "A")
 
     # "Very Good",        3.7,    "A-",        80,    84    ))
-    def test__A_minus__high(self):
+    def test084percent__A_minus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(84), "A-")
 
-    def test__A_minus__midrange(self):
+    def test082percent__A_minus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(82), "A-")
 
-    def test__A_minus__low(self):
+    def test080percent__A_minus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(80), "A-")
 
     # "Good",                3.3,    "B+",        77,    79    ))
-    def test__B_plus__high(self):
+    def test079percent__B_plus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(79), "B+")
 
-    def test__B_plus__midrange(self):
+    def test078percent__B_plus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(78), "B+")
 
-    def test__B_plus__low(self):
+    def test077percent__B_plus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(77), "B+")
 
     # "Good",                3.0,    "B",        73,    76    ))
-    def test__B__high(self):
+    def test076percent__B__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(76), "B")
 
-    def test__B__midrange(self):
+    def test075percent__B__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(75), "B")
 
-    def test__B__low(self):
+    def test073percent__B__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(73), "B")
 
     # "Good",                2.7,    "B-",        70,    72    ))
-    def test__B_minus__high(self):
+    def test072percent__B_minus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(72), "B-")
 
-    def test__B_minus__midrange(self):
+    def test071percent__B_minus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(71), "B-")
 
-    def test__B_minus__low(self):
+    def test070percent__B_minus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(70), "B-")
 
     # "Satisfactory",        2.3,    "C+",        67,    69    ))
-    def test__C_plus__high(self):
+    def test069percent__C_plus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(69), "C+")
 
-    def test__C_plus__midrange(self):
+    def test068percent__C_plus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(68), "C+")
 
-    def test__C_plus__low(self):
+    def test067percent__C_plus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(67), "C+")
 
     # "Satisfactory",        2.0,    "C",        63,    66    ))
-    def test__C__high(self):
+    def test066percent__C__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(66), "C")
 
-    def test__C__midrange(self):
+    def test065percent__C__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(65), "C")
 
-    def test__C__low(self):
+    def test063percent__C__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(63), "C")
 
     # "Satisfactory",        1.7,    "C-",        60,    62    ))
-    def test__C_minus__high(self):
+    def test062percent__C_minus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(62), "C-")
 
-    def test__C_minus__midrange(self):
+    def test061percent__C_minus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(61), "C-")
 
-    def test__C_minus__low(self):
+    def test060percent__C_minus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(60), "C-")
 
     # "Marginal Pass",    1.3,    "D+",        57,    59    ))
-    def test__D_plus__high(self):
+    def test059percent__D_plus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(59), "D+")
 
-    def test__D_plus__midrange(self):
+    def test580percent__D_plus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(58), "D+")
 
-    def test__D_plus__low(self):
+    def test057percent__D_plus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(57), "D+")
 
     # "Marginal Pass",    1.0,    "D",        53,    56    ))
-    def test__D__high(self):
+    def test056percent__D__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(56), "D")
 
-    def test__D__midrange(self):
+    def test055percent__D__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(55), "D")
 
-    def test__D__low(self):
+    def test053percent__D__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(53), "D")
 
     # "Marginal Pass",    0.7,    "D-",        50,    52    ))
-    def test__D_minus__high(self):
+    def test052percent__D_minus__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(52), "D-")
 
-    def test__D_minus__midrange(self):
+    def test051percent__D_minus__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(51), "D-")
 
-    def test__D_minus__low(self):
+    def test050percent__D_minus__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(50), "D-")
 
     # "Failure",            0.0,    "F",        0,    49    ))
-    def test__F__high(self):
+    def test049percent__F__high(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(49), "F")
 
-    def test__F__midrange(self):
+    def test025percent__F__midrange(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(25), "F")
 
-    def test__F__low(self):
+    def test000percent__F__low(self):
         self.assertEqual(gpa_calculator.gpa_converter.getLetterForNumericGrade(0), "F")
 
 
