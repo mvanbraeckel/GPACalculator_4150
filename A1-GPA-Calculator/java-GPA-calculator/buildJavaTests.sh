73824932672d6957cb1ea5d390cc114adc5c8920
mvanbraeckel <mvanbrae@uoguelph.ca>
2021-11-15 00:35:21 -0500

    adding tests for A2 GPA calculator states

--- A1-GPA-Calculator/java-GPA-calculator/buildJavaTests.sh
+++ A1-GPA-Calculator/java-GPA-calculator/buildJavaTests.sh
@@ -3,5 +3,5 @@
 set -x
 
 # You can modify this line if you wish
-javac -cp GPAcalculator.jar:junit-4.10.jar:. RunTests.java TestConversions.java
+javac -cp GPAcalculator.jar:junit-4.10.jar:. RunTests.java TestConversions.java TestGPACalculations.java
 
